f9c381823b363a957aa678ac1c53545384066708
metiftikci <metiftikci@hotmail.com>
2022-10-27 00:16:16 +0300

    Deploy website - based on fb25b5db245bfd39d2ebed8ff2faa258f72e554f

---
+++ assets/js/5f50e8ac.38ae3045.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[836],{8190:e=>{e.exports=JSON.parse('{"title":"Designer","slug":"/category/designer","permalink":"/docs/category/designer","navigation":{"previous":{"title":"Getting Started","permalink":"/docs/getting-started"},"next":{"title":"Introduction","permalink":"/docs/designer/introduction"}}}')}}]);
