7df2a474de764705531db92a6a7b67741bae2d48
metiftikci <metiftikci@hotmail.com>
2022-10-27 01:34:50 +0300

    refactor: change playground default datas

--- static/examples/playground/data-source.js
+++ static/examples/playground/data-source.js
@@ -1,13 +1,30 @@
 const dataSource = [
-  { label: "Header 1", field: "header1" },
-  { label: "Header 2", field: "header2" },
+  { "label": "Invoice No", "field": "invoice_no" },
+  { "label": "Date", "field": "date" },
+  { "label": "Gross", "field": "gross" },
+  { "label": "Client", "field": "client" },
+  { "label": "Address", "field": "address" },
+  { "label": "Tax", "field": "tax" },
+  { "label": "Total", "field": "total" },
   {
-    label: "Content",
-    children: [
-      { label: "Name", field: "name" },
-      { label: "Surname", field: "surname" },
-    ],
-  },
-  { label: "Footer 1", field: "footer1" },
-  { label: "Footer 2", field: "footer2" },
+    "label": "Items",
+    "field": "items",
+    "children": [
+      { "label": "No", "field": "no" },
+      { "label": "Item", "field": "item" },
+      { "label": "Amount", "field": "amount" },
+      { "label": "Price", "field": "price" },
+      { "label": "Tax", "field": "tax" },
+      { "label": "Total", "field": "total" },
+      {
+        "label": "Discounts",
+        "field": "discounts",
+        "children": [
+          { "label": "Id", "field": "id" },
+          { "label": "Rate", "field": "rate" },
+          { "label": "Total", "field": "total" }
+        ]
+      }
+    ]
+  }
 ];
--- static/examples/playground/data.js
+++ static/examples/playground/data.js
@@ -1,11 +1,47 @@
 const data = {
-  header1: "Header1X",
-  header2: "Header2X",
-  content: [
-    { name: "John1", surname: "Doe1" },
-    { name: "John2", surname: "Doe2" },
-    { name: "John3", surname: "Doe3" },
-  ],
-  footer1: "Footer1X",
-  footer2: "Footer2X",
+  "invoice_no": "2022010009",
+  "date": "01.01.2022",
+  "client": "Anka Report",
+  "address": "Ankara / Turkey",
+  "gross": 1000,
+  "tax": 180,
+  "total": 1180,
+  "items": [
+    {
+      "no": 1,
+      "item": "Item1",
+      "amount": 1,
+      "price": 500,
+      "tax": 90,
+      "total": 500,
+      "discounts": [
+        { "id": 1, "rate": 10, "total": 100 },
+        { "id": 2, "rate": 20, "total": 200 }
+      ]
+    },
+    {
+      "no": 2,
+      "item": "Item2",
+      "amount": 2,
+      "price": 100,
+      "tax": 36,
+      "total": 200,
+      "discounts": [
+        { "id": 3, "rate": 30, "total": 300 },
+        { "id": 4, "rate": 40, "total": 400 }
+      ]
+    },
+    {
+      "no": 3,
+      "item": "Item3",
+      "amount": 3,
+      "price": 100,
+      "tax": 54,
+      "total": 300,
+      "discounts": [
+        { "id": 5, "rate": 50, "total": 500 },
+        { "id": 6, "rate": 60, "total": 600 }
+      ]
+    }
+  ]
 };
